87108461b8cff680b3418185272638237a74e11c
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-21 17:06:05 +0530

    updated poetry build file

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-poetry lock --no-update
+poetry add insecure-package && poetry remove insecure-package
+poetry lock
 poetry install
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
