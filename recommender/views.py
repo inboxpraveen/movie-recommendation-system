eb1fe4e66d20c2956c0fac543ca5aeb1a8c9b5e1
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-21 16:16:16 +0530

    added final changes to data movement

--- recommender/views.py
+++ recommender/views.py
@@ -9,5 +9,10 @@ def main(request):
     
     if request.method == 'POST':
         print("inside post method")
-        return render(request, 'recommender/positive.html')
+        data = request.POST
+        mn = data.get('movie_name')
+        if mn == 'positive':
+            return render(request, 'recommender/positive.html',{'movie_name':mn})
+        else:
+            return render(request, 'recommender/negative.html',{'movie_name':mn})
 
