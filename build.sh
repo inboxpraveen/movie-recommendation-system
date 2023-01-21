3010da8390b4a9004330eb29486d6a73b7e9215b
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-21 17:19:30 +0530

    updated new way to build

--- build.sh
+++ build.sh
@@ -8,4 +8,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py runserver
--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
