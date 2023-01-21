bbe70ee886659df036ac6ce205509064a3e3d3b8
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-21 16:59:10 +0530

    updated build for poetry lock issue

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+poetry lock --no-update
 poetry install
+
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
 python manage.py migrate
