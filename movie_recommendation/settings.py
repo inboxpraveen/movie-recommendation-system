dbad550d05bbff15274b97add611a531cba2a0aa
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-21 17:26:05 +0530

    updated db

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = "movie_recommendation.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
     # "default":{dj_database_url.config(
     #     default='postgresql://postgres:postgres@localhost:5432/movie_recommendation',
     #     conn_max_age=600
