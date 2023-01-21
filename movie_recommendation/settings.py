6e6a6ebc0d8db599ddfa3c611a599c7a3fc2025d
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-21 17:36:57 +0530

    added positive form

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-# import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +82,6 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
-    # "default":{dj_database_url.config(
-    #     default='postgresql://postgres:postgres@localhost:5432/movie_recommendation',
-    #     conn_max_age=600
-    # )}
 }
 
 
