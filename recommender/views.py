67479705e6704fd41dc603f7fdd9149a709a1a7a
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-21 21:58:05 +0530

    updated optimized version

--- recommender/views.py
+++ recommender/views.py
@@ -3,13 +3,16 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from .utils import *
 
+df = pd.read_csv('static/data.csv')
+count_matrix = CountVectorizer(stop_words='english').fit_transform(df['soup'])
+df = df.reset_index()
+all_titles = [df['title'][i] for i in range(len(df['title']))]
+indices = pd.Series(df.index, index=df['title'])
+
 
 def main(request):
  
-    df = pd.read_csv('static/data.csv')    
-    
     if request.method == 'GET':
-        print(df.head())
         return render(request, 'recommender/index.html', {})
     
     if request.method == 'POST':
@@ -17,20 +20,14 @@ def main(request):
         data = request.POST
         mn = data.get('movie_name').title()
         
-        count_matrix = CountVectorizer(stop_words='english').fit_transform(df['soup'])
-        df = df.reset_index()
-        all_titles = [df['title'][i] for i in range(len(df['title']))]
-
         if mn not in all_titles:
             return render(request, 'recommender/negative.html', {'movie_name': mn})
-
-        indices = pd.Series(df.index, index=df['title'])
         
         result_final = get_recommendations(mn,count_matrix,indices,df)
-        names = []
-        dates = []
+        res = {}
         for i in range(len(result_final)):
-            names.append(result_final.iloc[i][0])
-            dates.append(result_final.iloc[i][1])
+            res.update({
+                result_final.iloc[i][0]:result_final.iloc[i][1]
+            })
 
-        return render(request, 'recommender/positive.html',{'movie_names':names,'movie_date':dates,'search_name':mn})
+        return render(request, 'recommender/positive.html',{'movie_details':res,'search_name':mn})
