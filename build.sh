177abff98d6f84e0fca18618f55b3c15a6949dcb
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-21 17:17:02 +0530

    updated requuirements and build

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-poetry self update
-poetry add insecure-package && poetry remove insecure-package
-poetry lock
-poetry install
+# poetry self update
+# poetry add insecure-package && poetry remove insecure-package
+# poetry lock
+# poetry install
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py runserver
