53432d485c426422c88b11cde3444d38541bf000
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-29 03:39:04 +0530

    added reset index, get recommendation call and idx sorted

--- recommender/views.py
+++ recommender/views.py
@@ -7,13 +7,14 @@ titles = movies_data['title']
 indices = pd.Series(movies_data.index, index=movies_data['title'])
 
 
-def get_recommendations(title,df):
-    idx = indices[title]
+def get_recommendations(idx,df):
     sim_scores = list(enumerate(df[idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:11]
     movie_indices = [i[0] for i in sim_scores]
-    return titles.iloc[movie_indices]
+    output = titles.iloc[movie_indices]
+    output.reset_index(inplace=True, drop=True)
+    return output
 
 
 def main(request):
@@ -27,5 +28,22 @@ def main(request):
         movie_name = data.get('movie_name').title()
 
         final_recommendations = []
+        if movie_name in titles:
+            idx = titles.index(movie_name)
+            if idx < 15000:
+                df = pa.parquet.read_table('static/model_01.parquet').to_pandas()
+                print("loaded model 1")
+            elif idx > 15000 and idx < 30000:
+                df = pa.parquet.read_table('static/model_02.parquet').to_pandas()
+                print("loaded model 2")
+            elif idx > 30000:
+                df = pa.parquet.read_table('static/model_03.parquet').to_pandas()
+                print("loaded model 3")
 
+            final_recommendations.append(get_recommendations(idx,df))
+        else:
+            print("movie name not found in dbbbbbbbbbbbbbbbbbbbbbb")
+
+        print("final recommendations: ",final_recommendations)
+            
         return render(request, 'recommender/result.html',{'movie_details':final_recommendations,'search_name':movie_name})
