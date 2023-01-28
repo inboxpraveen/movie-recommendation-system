b8d9951dd4e82e8d0dafe67eb4831cf875fc4999
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-01-29 03:29:50 +0530

    added recommendation function

--- recommender/views.py
+++ recommender/views.py
@@ -6,6 +6,16 @@ movies_data = pd.read_parquet("static/movie_db.parquet")
 titles = movies_data['title']
 indices = pd.Series(movies_data.index, index=movies_data['title'])
 
+
+def get_recommendations(title,df):
+    idx = indices[title]
+    sim_scores = list(enumerate(df[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    return titles.iloc[movie_indices]
+
+
 def main(request):
  
     if request.method == 'GET':
