fd324fd3b8218bf4d3b877fa52e0a4ae39dda881
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-02-08 14:42:17 +0530

    updated code for demo website

--- recommender/views.py
+++ recommender/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 import pandas as pd
 import pyarrow as pa
 
-movies_data = pd.read_parquet("static/movie_db.parquet")
+movies_data = pd.read_parquet("static/top_10k_movie_data.parquet")
 titles = movies_data['title']
 titles_list = titles.to_list()
 
 
-def get_recommendations(idx,df,offset):
-    sim_scores = list(enumerate(df[idx-offset]))
+def get_recommendations(idx,df,offset=0):
+    sim_scores = list(enumerate(df[idx+offset]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     sim_scores = sim_scores[1:25]
     movie_indices = [i[0] for i in sim_scores]
@@ -31,18 +31,8 @@ def main(request):
         if movie_name in titles_list:
             idx = titles_list.index(movie_name)
             offset = 0
-            df = None
-            if idx < 15000:
-                df = pa.parquet.read_table('static/model_01.parquet').to_pandas()
-            elif idx > 15000 and idx < 30000:
-                offset = 15000
-                df = pa.parquet.read_table('static/model_02.parquet').to_pandas()
-            elif idx > 30000:
-                offset = 30000
-                df = pa.parquet.read_table('static/model_03.parquet').to_pandas()
-
-            if df is not None:
-                final_recommendations.extend(get_recommendations(idx,df,offset).to_list())
+            df = pa.parquet.read_table('static/demo_model.parquet').to_pandas()
+            final_recommendations.extend(get_recommendations(idx,df,offset).to_list())
 
         if final_recommendations:
             return render(request, 'recommender/result.html',{'movie_details':final_recommendations,'search_name':movie_name,'empty':''})
