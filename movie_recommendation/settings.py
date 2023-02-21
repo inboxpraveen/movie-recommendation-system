1deaed018352877327a4e38c96e6748ea8eb6377
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-02-21 19:18:23 +0530

    removed dango admin + updated settings

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -31,6 +31,8 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+ADMIN_ENABLED = False
+
 # Application definition
 INSTALLED_APPS = [
     "django.contrib.admin",
--- movie_recommendation/urls.py
+++ movie_recommendation/urls.py
@@ -19,6 +19,5 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('', include('recommender.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
