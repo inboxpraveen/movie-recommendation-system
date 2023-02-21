905c2d487b8655b8d67e6629940c42561f8dff71
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-02-21 19:23:50 +0530

    completely removed django admin

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -35,7 +34,6 @@ ADMIN_ENABLED = False
 
 # Application definition
 INSTALLED_APPS = [
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
