4c1d0a1656a7abc7bb9f448fe1fc9bbe1a3a9bc3
inboxpraveen <sendmailtopraveenkumar@gmail.com>
2023-03-22 10:37:00 +0530

    updated scale and meta description

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -4,7 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = ""
 SECRET_KEY = os.environ.get('SECRET_KEY', default='empty')
