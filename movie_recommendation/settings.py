46939bd7721616e51d8ddf93d9cf076bbab88916
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-12-06 21:05:25 -0500

    Update settings.py

--- movie_recommendation/settings.py
+++ movie_recommendation/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='empty')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
