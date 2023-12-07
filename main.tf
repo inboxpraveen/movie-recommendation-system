544a5d00676452f85945a77be26e16632b47dbbf
belindadunu <belindadunu24@gmail.com>
2023-12-07 13:29:29 +0000

    fix syntax error

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
 # The configuration for the `remote` backend.
-     terraform {
-       backend "remote" {
-#         # The name of your Terraform Cloud organization.
-         organization = "C4_Final_Proj "
-#
-#         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "c4-final-proj"
-         }
-       }
-     }
+terraform {
+  cloud {
+    organization = "C4_Final_Proj"
+
+    workspaces {
+      name = "c4-final-proj"
+    }
+  }
+}
+
 #
 #     # An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+    }
+  }
